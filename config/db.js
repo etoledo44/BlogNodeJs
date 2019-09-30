27f9bca36d0a77fa11cc743d62d74aba4d53d8ee
Érique Toledo <eriquetoledo@outlook.com>
2019-09-30 12:41:10 -0300

    add url mongo

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: 'mongodb+srv://eriquetoledo:123erock@cluster0-h4v5x.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://deploy:uploadapp@cluster0-h4v5x.mongodb.net/test?retryWrites=true&w=majority'}
 
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
